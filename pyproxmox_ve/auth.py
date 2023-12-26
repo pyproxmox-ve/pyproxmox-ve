c2520bc03f38695ecd781e0c539cd53232b26d24
Brandon <brandon-spendlove@hotmail.co.uk>
2023-12-26 08:18:59 +0000

    need to merge /access/ticket implementation for cookie auth (#20)

--- pyproxmox_ve/auth.py
+++ pyproxmox_ve/auth.py
@@ -1,6 +1,11 @@
 from aiohttp import BasicAuth
 
 
+class PVECookieTokenAuth(BasicAuth):
+    def encode(self) -> str:
+        return f"PVEAuthCookie={self.login}"
+
+
 class PVEAPITokenAuth(BasicAuth):
     def encode(self) -> str:
         return f"PVEAPIToken={self.login}={self.password}"
